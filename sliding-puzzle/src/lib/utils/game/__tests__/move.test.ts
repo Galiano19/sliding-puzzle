af3b608fded754b1094fff7b62c82c781ef41f23
Carlos Galiano <galihorizon21@gmail.com>
2026-02-18 12:14:47 +0100

    fix failing tests

--- sliding-puzzle/src/lib/utils/game/__tests__/move.test.ts
+++ sliding-puzzle/src/lib/utils/game/__tests__/move.test.ts
@@ -11,12 +11,12 @@ describe("moveTile", () => {
   describe("move strategy", () => {
     it("returns the same state if tile to move is not adjecent to an empty space", () => {
       const result = moveTile(board, 0);
-      expect(result).toEqual([1, 2, 3, 4, 5, 6, 7, 8, null]);
+      expect(result.tiles).toEqual([1, 2, 3, 4, 5, 6, 7, 8, null]);
     });
 
     it("moves tile adjacent to empty space", () => {
       const result = moveTile(board, 5);
-      expect(result).toEqual([1, 2, 3, 4, 5, null, 7, 8, 6]);
+      expect(result.tiles).toEqual([1, 2, 3, 4, 5, null, 7, 8, 6]);
     });
   });
 
